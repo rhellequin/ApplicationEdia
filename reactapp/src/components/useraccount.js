fa4035f35f85038f0645c7e62d039e46dcd0f1f1
Cyprien CREPEAUX <cyprien.crepeaux@gmail.com>
2021-07-28 12:03:40 +0200

    infoperso

--- reactapp/src/components/useraccount.js
+++ reactapp/src/components/useraccount.js
@@ -1,33 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import { Input, Typography, Space } from 'antd';
-import { Button,Col,Row,Container,Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Media} from 'reactstrap';
-import{Link,Redirect} from "react-router-dom";
-import {connect} from 'react-redux';
+import { Input, Typography, Space, Form, Button } from 'antd';
+import { Container, Col } from 'reactstrap';
+import { Link, Redirect } from "react-router-dom";
+import { connect } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from 'antd/lib/layout/layout';
 import Nav from './nav';
+import { BulkWriteError } from 'mongodb';
+
+function UserAccount() {
+
+        return (
+            <Container>
+                <Nav />
+
+                <Col>
+                <h1>Mes informations personnelles</h1>
+
+                    <Form>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Prénom" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Nom" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Téléphone" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Mail" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Entreprise" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Siret" />
+                        </Form.Item>
+                        <Form.Item style={{width:'345px'}} label="">
+                            <Input placeholder="Fonction" />
+                        </Form.Item>
+                        <Form.Item>
+                            <Button style={{width:'100px',  background: "#0A62D0" }} type="primary">Enregister</Button>
+                        </Form.Item>
+                    </Form>
+
+                </Col>
 
 
+            </Container>
+        )
 
+    };
 
-function UserAccount () {
-
-
-return (
-    <Container>
-    <Nav/>
-        <Col>
-        
-        
-        </Col>
-
-
-    </Container>
-)
-
-
-}
-
-export default UserAccount
-
+    export default UserAccount
 
